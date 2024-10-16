4c446337980482cd5982cfe642ba9dfdd2594387
Rohit823823 <sahurohitkumar2001@gmail.com>
2024-10-16 07:49:37 +0530

    Commmit No1 on 16 oct 2024

---
+++ src/Oct/ex_16102024/Lab_010.java
@@ -0,0 +1,22 @@
+package Oct.ex_16102024;
+import java.util.Scanner;
+public class Lab_010 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter your name");
+        String name=sc.nextLine();
+        System.out.println("Enter your age");
+        int age=sc.nextInt();
+        System.out.println("Enter your salary");
+        int salary=sc.nextInt();
+        sc.close();
+
+    }
+}
+// Output:-
+// Enter your name
+//Rohit Sahu
+//Enter your age
+//23
+//Enter your salary
+//50,000
