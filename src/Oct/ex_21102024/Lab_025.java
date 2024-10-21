7c20eb8c64a5c62eaa0d17ca47d5bf3de400b6ae
Rohit823823 <sahurohitkumar2001@gmail.com>
2024-10-21 09:08:41 +0530

    Commit on (26) 21 oct 2024

--- src/Oct/ex_21102024/Lab_025.java
+++ src/Oct/ex_21102024/Lab_025.java
@@ -3,12 +3,12 @@ package Oct.ex_21102024;
 public class Lab_025 {
     public static void main(String[] args) {
         int age = 10;
-        System.out.printf("Your age is %d",age);
+        System.out.printf("Hii Your age is %d",age);
 
         String name  = "rohit";
-        System.out.printf("Hi, Your name is %s",name);
+        System.out.printf("Hii Your name is %s",name);
 
         boolean b = true;
-        System.out.printf("Hi, %b",b);
+        System.out.printf("Hii, %b",b);
     }
 }
