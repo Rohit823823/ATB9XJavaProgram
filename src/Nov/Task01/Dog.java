6d7ca0969b45d2ec800ee073f239c052452ed5d0
Rohit823823 <sahurohitkumar2001@gmail.com>
2024-11-05 11:27:46 +0530

    Commit no (Task-1{Dog,Runner}) on 5 november 2024

---
+++ src/Nov/Task01/Dog.java
@@ -0,0 +1,21 @@
+package Nov.Task01;
+
+public class Dog {
+    // ATTRIBUTES FOR DOG
+    String name;
+    String colour;
+    int age;
+    double weight;
+    String breed;
+    public void displayInfo() {
+        System.out.println("Name: " +name);
+        System.out.println("Colour: " +colour);
+        System.out.println("Age: " +age);
+        System.out.println("Weight: " +weight);
+        System.out.println("Breed: " +breed);
+        System.out.println();
+    }
+
+
+
+}
