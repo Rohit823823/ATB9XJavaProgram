669662e770c494ef4c98c564d944fea091b3beb1
Rohit823823 <sahurohitkumar2001@gmail.com>
2024-11-12 09:09:40 +0530

    Commit all files i.e (155,156,157,158,159,160,161,162,Tesla) on 11 november 2024

---
+++ src/Nov/ex_12112024/Lab_156.java
@@ -0,0 +1,13 @@
+package Nov.ex_12112024;
+
+public class Lab_156 {
+}
+class A {
+    class B {
+        class C {
+
+        }
+    }
+}
+
+
---
+++ src/Nov/ex_12112024/Lab_159.java
@@ -0,0 +1,40 @@
+package Nov.ex_12112024;
+
+public class Lab_159 {
+    public static void main(String[] args) {
+        WagonR r  = new WagonR();
+        r.drive();
+
+        Tesla t  = new Tesla();
+        t.drive();
+
+        // Engine e = new Engine();
+    }
+}
+
+class WagonR extends Engine{
+
+    void drive(){
+        startEngine();
+        stopEngine();
+    }
+
+
+
+    @Override
+    void startEngine() {
+        System.out.println("starting car");
+    }
+
+    @Override
+    void stopEngine() {
+        System.out.println("stopping car");
+    }
+}
+
+
+
+abstract class Engine {
+    abstract void startEngine();
+    abstract void stopEngine();
+}
